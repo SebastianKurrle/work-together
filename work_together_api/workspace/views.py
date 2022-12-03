d4b3c925973150a9558dd1089d8e8d35e1fc83a9
= <=>
2022-12-04 00:57:23 +0100

    fnishing worksapce creation

--- work_together_api/workspace/views.py
+++ work_together_api/workspace/views.py
@@ -1,11 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import WorkspaceSerializer
+from .models import Workspace
 
 class CreateWorkspace(APIView):
     def post(self, request, format=None):
         serializer = WorkspaceSerializer(data=request.data)
-        print(serializer.is_valid(raise_exception=True))
-        serializer.save()
+        serializer.is_valid()
+        workspace_name = serializer.validated_data.get('name')
+        if Workspace.objects.filter(name=workspace_name.lower()).exists():  
+            return Response({'error' : 'A workspace with this name already exists'})
 
+        serializer.save()
         return Response(status=200)
