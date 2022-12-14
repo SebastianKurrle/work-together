be53c99f22db034a8136b9656b7edd16588d9e20
= <=>
2022-12-14 17:25:03 +0100

    implemented to join orgs

--- work_together_api/organization/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class OrganizationIsOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        print(obj.owner == request.user)
-        return obj.owner == request.user
